dbc8e97a4a521c2ea5647b560a6d6cdb5571e35c
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2015-08-19 13:04:34 +0300

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "pycluster",
-      version = "0.0.1",
+      version = "0.0.2",
       py_modules = ["pycluster", "pyclusterMainScripts"],
       entry_points =  {
           "console_scripts" : [
